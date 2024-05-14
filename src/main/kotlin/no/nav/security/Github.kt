2412839c3d3df883f71a203c25b6c12e9402ce7a
albrektsson <emil.albrektsson@nav.no>
2024-05-14 15:52:24 +0200

    Take two on pagination, added check for rate limit

--- src/main/kotlin/no/nav/security/Github.kt
+++ src/main/kotlin/no/nav/security/Github.kt
@@ -17,26 +17,35 @@ class GitHub(
     )
 
     suspend fun fetchStatsForBigQuery(): List<IssueCountRecord> {
-        logger.info("Built ${records.size} records from GitHub response")
         fetchDataFromGraphql()
+        logger.info("Built ${records.size} records from GitHub response")
         return records
     }
 
     private tailrec suspend fun fetchDataFromGraphql(
         teamCursor: String? = null, repoCursor: String? = null, vulnCursor: String? = null
-    ): List<IssueCountRecord>? {
+    ) {
         val ghQuery = FetchGithubStatsQuery(
-            variables = FetchGithubStatsQuery.Variables(orgName = "navikt", teamCursor = teamCursor, repoCursor = repoCursor, vulnCursor = vulnCursor)
+            variables = FetchGithubStatsQuery.Variables(
+                orgName = "navikt",
+                teamCursor = teamCursor,
+                repoCursor = repoCursor,
+                vulnCursor = vulnCursor
+            )
         )
         val response: GraphQLClientResponse<FetchGithubStatsQuery.Result> = client.execute(ghQuery)
-
         response.errors?.let {
             logger.error("Error fetching data from GitHub: $it")
             throw RuntimeException("Error fetching data from GitHub: $it")
         }
 
-        response.data?.organization?.teams?.nodes?.map { team ->
-            team?.repositories?.nodes?.map { repo ->
+        if(response.data?.rateLimit?.remaining!! < 100) {
+            logger.error("Rate limit is low: ${response.data?.rateLimit?.remaining}")
+            throw RuntimeException("Rate limit is low (<100)")
+        }
+
+        response.data?.organization?.teams?.nodes?.forEach { team ->
+            team?.repositories?.nodes?.forEach { repo ->
                 repo?.let {
                     records.add(
                         IssueCountRecord(
@@ -49,48 +58,32 @@ class GitHub(
                         )
                     )
                 }
+
+                val vulnPageInfo = repo?.vulnerabilityAlerts?.pageInfo
+                val nextVulnPage = vulnPageInfo?.endCursor.takeIf { vulnPageInfo?.hasNextPage ?: false }
+
+                if (nextVulnPage != null) {
+                    return fetchDataFromGraphql(
+                        teamCursor = teamCursor,
+                        repoCursor = repoCursor,
+                        vulnCursor = nextVulnPage
+                    )
+                }
+            }
+
+            val repoPageInfo = team?.repositories?.pageInfo
+            val nextRepoPage = repoPageInfo?.endCursor.takeIf { repoPageInfo?.hasNextPage ?: false }
+
+            if (nextRepoPage != null) {
+                return fetchDataFromGraphql(teamCursor = teamCursor, repoCursor = nextRepoPage)
             }
         }
 
-        val lastTeam = response.data?.organization?.teams?.nodes?.lastOrNull()?.name
-        val lastRepo = response.data?.organization?.teams?.nodes?.find { it?.name == lastTeam }?.repositories?.nodes?.lastOrNull()?.name
+        val teamPageInfo = response.data?.organization?.teams?.pageInfo
+        val nextTeamPage = teamPageInfo?.endCursor.takeIf { teamPageInfo?.hasNextPage ?: false }
 
-        val haveNextTeamPage = response.fetchTeamsCursor()
-        val haveNextRepoPage = lastTeam?.let { response.fetchReposCursor(it) }
-        val haveNextVulnPage = lastTeam?.let { lastRepo?.let { response.fetchVulnCursor(lastTeam, lastRepo) } }
-
-        // If any of the cursors are not null, there are more pages to fetch
-        // If we have vulnCursor we use that, if not we use repoCursor, if not we use teamCursor
-        val nextVulnPage = haveNextVulnPage
-        val nextRepoPage = if(haveNextVulnPage == null) haveNextRepoPage else null
-        val nextTeamPage = if(haveNextVulnPage == null && haveNextRepoPage == null) haveNextTeamPage else null
-
-        val hasNextPage = nextVulnPage != null || nextRepoPage != null || nextTeamPage != null
-
-        return if(hasNextPage) fetchDataFromGraphql(teamCursor = nextTeamPage, repoCursor = nextRepoPage, vulnCursor = nextVulnPage) else null
+        if (nextTeamPage != null) {
+            fetchDataFromGraphql(teamCursor = nextTeamPage)
+        }
     }
-
-    private fun GraphQLClientResponse<FetchGithubStatsQuery.Result>.fetchTeamsCursor(): String? =
-        data?.organization?.teams?.pageInfo?.let { page ->
-                if(page.hasNextPage) page.endCursor
-                else null
-            }
-
-    private fun GraphQLClientResponse<FetchGithubStatsQuery.Result>.fetchReposCursor(repo: String): String? =
-        data?.organization?.teams?.nodes
-            ?.find { it?.name == repo }?.repositories?.pageInfo?.let {page ->
-                if(page.hasNextPage) page.endCursor
-                else null
-            }
-
-    private fun GraphQLClientResponse<FetchGithubStatsQuery.Result>.fetchVulnCursor(
-        team: String,
-        repo: String
-    ): String? =
-        data?.organization?.teams?.nodes
-            ?.find { it?.name == team }?.repositories?.nodes
-            ?.find { it?.name == repo }?.vulnerabilityAlerts?.pageInfo?.let { page ->
-                if(page.hasNextPage) page.endCursor
-                else null
-            }
 }
