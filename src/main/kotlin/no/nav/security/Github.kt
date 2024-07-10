7470338e393a05999632a4818b41e4ba91916299
J-K. Solbakken <jksolbakken@gmail.com>
2024-07-10 13:04:40 +0200

    put related stuff together

--- src/main/kotlin/no/nav/security/Github.kt
+++ src/main/kotlin/no/nav/security/Github.kt
@@ -47,5 +47,14 @@ class GitHub(
     }
 }
 
+class GithubRepository (
+    val id: ID,
+    val name: String,
+    val isArchived: Boolean,
+    val pushedAt: DateTime?,
+    val hasVulnerabilityAlertsEnabled: Boolean,
+    val vulnerabilityAlerts: Int,
+)
+
 typealias DateTime = String
 typealias ID = String
